16df8607d3e5e4b62c492b6013e17bd2eba1825b
berkeracir159@gmail.com <159.Berker>
2021-01-10 14:29:30 +0300

    add extract_verified

---
+++ src/main/python/extract_verified.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from glob import glob
+import os
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description="Extract data from troll csv")
+    parser.add_argument("archive_path", help="A root folder where the archive of tweets stored")
+    parser.add_argument("out_path", help="Output path for model")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    sc = SparkContext(master="local[*]", appName="extract_verified")
+    spark = SparkSession(sc)
+
+    for dayPath in glob(os.path.join(args.archive_path, "*")):
+        for dayHourPath in glob(os.path.join(dayPath, "*")):
+            splittedPath = dayHourPath.split("\\")
+            day = splittedPath[-2]
+            hour = splittedPath[-1]
+
+            print(f"Starting {day} - {hour}")
+
+            inputPath = os.path.join(dayHourPath, "*.json.bz2")
+            outPath = os.path.join(args.out_path, f"{day}_{hour}")
+            archived_tweets = spark.read.json(inputPath, encoding="utf-8")
+            out_tweets = archived_tweets.filter((archived_tweets.lang == "tr") &
+                                                archived_tweets.user.verified &
+                                                archived_tweets.retweeted_status.isNull())
+            out_tweets.select("text", "extended_tweet.full_text").write.csv(outPath, header=True, encoding="utf-8")
+
+            print(f"Finished {day} - {hour}")
+            break
+        break
+
+
+if __name__ == "__main__":
+    exit(main())
